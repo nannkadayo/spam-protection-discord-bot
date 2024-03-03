831725e05747d86a657ee4a64449e6457ea03dd9
nannkadayo <154228074+nannkadayo@users.noreply.github.com>
2024-03-03 12:48:02 +0900

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -8,7 +8,9 @@ export default {
     "admin": [""],
             //すべての荒らし対策を無視できるユーザー
     "owner": [""],
+    "logfilename": "state.log",
     "abusivelist":/しね|ころす|死ね|sine|殺す|きっしょ|ゴミ|野獣先輩|糞|カス|消えろ|生きる価値なし|きえろ|fuck|fxxk|ファック|ふぁっく|japs|jap/
+        
         },
    color:{
     "fatal": 0xff0000,
