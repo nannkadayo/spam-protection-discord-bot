a77abfb7cf9a353ca0fb31db3d42d4c0e92f2d2e
nannkadayo <154228074+nannkadayo@users.noreply.github.com>
2024-03-03 10:10:40 +0900

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -1,24 +1,13 @@
 export default {
     botdata:{
-    "token": "MTE5NzQ2MjQ3ODk4OTI5NTY3Ng.GKC0Ha.V2l6TzMMhAJz5od07kjJdjQjIIUMd-IwT-W8AY",
+    "token": "",
     "botname":"荒らし対策bot",
     "banmaxpoint": "10",
-    "log": ["1197454689172729877"],
+    "log": [""],
     //gbanやpoint設定できるユーザー
-    "admin": [// nannkadayo (@userdayo)
-             "1098082127343538278",
-             // uu (@uu.5007.mp)
-             "936504264149336064",
-             // シミ (@omoti1102)
-             "1150270759952982197",
-             // まっちゃぷりん (@siroooo_0627)
-             "1207611683380666368"
-            ],
+    "admin": [""],
             //すべての荒らし対策を無視できるユーザー
-    "owner": [
-        // nannkadayo (@userdayo)
-        "1098082127343538278"
-    ],
+    "owner": [""],
     "abusivelist":/しね|ころす|死ね|sine|殺す|きっしょ|ゴミ|野獣先輩|糞|カス|消えろ|生きる価値なし|きえろ|fuck|fxxk|ファック|ふぁっく|japs|jap/
         },
    color:{
